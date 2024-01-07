e0f2aa72a6362cb4cf2e27f05623db98d53a919e
David Vance <davidvance512@gmail.com>
2024-01-07 14:42:59 -0500

    More work on class implementation

--- particle.py
+++ particle.py
@@ -2,17 +2,6 @@ import random
 import numpy as np
 import pygame
 
-def new_matrix():
-    '''
-    Generates a matrix of random attraction and repulsion for each pair of colors between -1 and 1.
-    '''
-
-    attract_matrix = np.ndarray(shape=(6, 6), dtype=float)
-    for i in range(6):
-        for j in range(6):
-            attract_matrix[i][j] = random.random()*2 - 1  
-    return attract_matrix
-
 class Particle:
     red_count = 0
     green_count = 0
@@ -20,15 +9,15 @@ class Particle:
     purple_count = 0
     yellow_count = 0
     cyan_count = 0
-    COLORS=[(255,0,0),(0,255,0),(0,0,255),(255,0,255),(0,255,255),(255,255,0)]
-    attract_matrix = new_matrix()
+    COLORS=[(255,0,0), (0,255,0), (0,0,255), (255,0,255), (0,255,255), (255,255,0)]
     
-    def __init__(self):
-        self.x = random.random()
-        self.y = random.random()
+    def __init__(self, simulation_size_x, screen_size_y):
+        self.x = random.random() * simulation_size_x
+        self.y = random.random() * screen_size_y
         self.x_vel = 0
         self.y_vel = 0
-        self.size = random.choice([1,2,3,4,5,6])
+        self.size = 2
+        # self.size = random.choice([1,2,3,4,5,6])
         self.color = random.choice([0,1,2,3,4,5])
         if self.color == 0:
             Particle.red_count += 1
@@ -43,29 +32,38 @@ class Particle:
         elif self.color == 5:
             Particle.cyan_count += 1
 
-    def intra_particle_dist(self, other_particle, rmax, screen_size_y):
+    @staticmethod
+    def new_matrix():
+        '''
+        Generates a matrix of random attraction and repulsion for each pair of colors between -1 and 1.
+        '''
+        attract_matrix = np.ndarray(shape=(6, 6), dtype=float)
+        for i in range(6):
+            for j in range(6):
+                attract_matrix[i][j] = random.random()*2 - 1 
+        return attract_matrix 
+
+    def intra_particle_dist(self, other_particle):
         '''
         Determines the distance between two particles.
         '''
-        x_dist = screen_size_y*abs(self.x-other_particle.x)
-        y_dist = screen_size_y*abs(self.y-other_particle.y)
+        x_dist = abs(self.x-other_particle.x)
+        y_dist = abs(self.y-other_particle.y)
         r = (x_dist**2 + y_dist**2)**(1/2)
-        scaled_dist = r/rmax
-        print(self, other_particle, x_dist, y_dist, r, scaled_dist)
-        return x_dist, y_dist, r, scaled_dist
+        # print(self, other_particle, x_dist, y_dist)
+        return x_dist, y_dist, r
 
-    def force(self, other_particle, beta, rmax, screen_size_y):
+    def force(self, other_particle, r, rmax, beta):
         '''
         Determines the force applied by one particle on another based on their distance apart.
         '''
-        x_dist, y_dist, r, scaled_dist = self.intra_particle_dist(other_particle, rmax, screen_size_y)
+        scaled_dist = r/rmax
         if scaled_dist < beta:
-            f = 1 - (scaled_dist/beta)
+            return 1 - (scaled_dist/beta)
         elif scaled_dist < 1:
-            f =  Particle.attract_matrix[self.color][other_particle.color] * (1 - abs(2 * scaled_dist - 1 - beta)/ (1 - beta))
-        else:
-            f=0
-        return (x_dist/r) * f, (y_dist/r) * f
+            return Particle.attract_matrix[self.color][other_particle.color] * (1 - abs(2 * scaled_dist - 1 - beta)/ (1 - beta))
 
-    def draw(self, simulation_screen, simulation_size_x, screen_size_y):
-        pygame.draw.circle(simulation_screen, Particle.COLORS[self.color], (self.x*simulation_size_x, self.y*screen_size_y), self.size)  
+    def draw(self, simulation_screen):
+        pygame.draw.circle(simulation_screen, Particle.COLORS[self.color], (self.x, self.y), self.size)  
+
+    attract_matrix = new_matrix()
